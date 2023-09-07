a40f50e5f999a05f373fdbb8d5629e138093d613
yusei-mizuno <143696635+yusei-mizuno@users.noreply.github.com>
2023-09-07 19:13:39 +0900

    素数判定

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.Write("整数を入力:");
+int i, amari, count;
+i = 1;
+count = 0;
+int suji = int.Parse(Console.ReadLine());
+while (i <= suji)
+{
+    amari = suji % i;
+    if (amari == 0)
+    {
+        count++;
+    }
+    i++; ;
+}
+
+if (count == 2) 
+   {
+    Console.WriteLine("これは素数です");
+    }
+else if (count > 2) 
+   { 
+    Console.WriteLine("これは素数ではないです");
+   }
